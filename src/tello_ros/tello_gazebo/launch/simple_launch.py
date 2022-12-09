30b361d4f2685fd4dbf5eaaf015e46b42876c64a
RobKenobi <robinson.vaitilingom@outlook.fr>
2022-12-09 13:07:06 +0100

    new world with drone in it

--- src/tello_ros/tello_gazebo/launch/simple_launch.py
+++ src/tello_ros/tello_gazebo/launch/simple_launch.py
@@ -5,7 +5,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, TimerAction
 
 
 def generate_launch_description():
@@ -24,16 +24,28 @@ def generate_launch_description():
         ], output='screen'),
 
         # Spawn tello.urdf
-        Node(package='tello_gazebo', executable='inject_entity.py', output='screen',
-             arguments=[urdf_path, '0', '0', '1', '0']),
+        TimerAction(
+           period = 5.0,
+           actions = [
+                Node(
+                     package = "gazebo_ros",
+                     executable = "spawn_entity.py",
+                     arguments = [
+                          "-entity", "drone_ipsa",
+                          "-topic", "/robot_description",
+                          "-x", "0.0"
+                     ]
+                )
+           ]
+        ),
 
         # Publish static transforms
         Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen',
              arguments=[urdf_path]),
 
         # Joystick driver, generates /namespace/joy messages
-        Node(package='joy', executable='joy_node', output='screen',
-             namespace=ns),
+        #Node(package='joy', executable='joy_node', output='screen',
+        #     namespace=ns),
 
         # Joystick controller, generates /namespace/cmd_vel messages
         Node(package='tello_driver', executable='tello_joy_main', output='screen',
